992f533cd4d8b03b10b8f1c79777bcb9ebfcdfb2
Philip Guedes <philipguedes@gmail.com>
2019-08-30 03:47:25 -0300

    remove old routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
-import { BasicInfos } from './pages/basic-infos';
 import { EventPage } from './pages/event';
 import { Feed } from './pages/feed';
 import Registration from './pages/registration';
-import { SeriousnessMap } from './pages/seriousness-map';
 import { ProfessionalsPage } from './pages/professional';
 
 function App() {
@@ -13,10 +11,8 @@ function App() {
     <div className="app">
 
       <Router>
-        <Route path="/" exact component={Feed} />
         <Route path="/registration" component={Registration} />
-        <Route path="/registration0" component={BasicInfos} />
-        <Route path="/registration1" component={SeriousnessMap} />
+        <Route path="/" exact component={Feed} />
         <Route path="/eventos/:name" component={EventPage} />
         <Route path="/profissionais/:name" component={ProfessionalsPage} />
       </Router>
